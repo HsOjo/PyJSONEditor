5abfa8b00e8b1ed612ef42795e740ef3e76e9d1a
HsOjo <1134031392@qq.com>
2019-08-06 04:14:25 +0800

    Fix icon.

--- build.py
+++ build.py
@@ -47,7 +47,13 @@ for k, v in datas.items():
     data_str += '--add-data "%s:%s"' % (k, v)
 
 common.log('Build', 'Info', 'Pyinstaller packing now...')
-pyi_cmd = 'pyinstaller -F -w -n "%s" -i "./app/res/icon.png" %s \\\n__main__.py' % (Const.app_name, data_str)
+
+if sys_type == 'Darwin':
+    path_icon = './app/res/icon.icns'
+else:
+    path_icon = './app/res/icon.ico'
+
+pyi_cmd = 'pyinstaller -F -w -n "%s" -i "%s" %s \\\n__main__.py' % (Const.app_name, path_icon, data_str)
 print(pyi_cmd)
 os.system(pyi_cmd)
 os.unlink('./%s.spec' % Const.app_name)
@@ -64,6 +70,6 @@ if sys_type == 'Darwin':
             path = os.path.join(d, f)
             z_path = path[7:].strip(os.path.sep)
             zf.write(path, z_path)
-            zf.close()
+        zf.close()
 
 common.log('Build', 'Info', 'Build finish.')
